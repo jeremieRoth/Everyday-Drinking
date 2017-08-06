0af36ac976896b363593caa340d92786e33beb9e
gilian dupuis <gilian.dupuis@orange.fr>
2017-08-06 14:48:58 +0200

    add services and entity

---
+++ app/src/main/java/com/example/gilian/bars_coop/Entity/User.java
@@ -0,0 +1,10 @@
+package com.example.gilian.bars_coop.Entity;
+
+/**
+ * Created by Gilian on 06/08/2017.
+ */
+
+public class User
+{
+
+}
---
+++ app/src/main/java/com/example/gilian/bars_coop/services/userService.java
@@ -0,0 +1,39 @@
+package com.example.gilian.bars_coop.services;
+
+//Dependencies
+import java.util.List;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.Field;
+
+//Entities
+import com.example.gilian.bars_coop.Entity.User;
+
+/**
+ * Created by Gilian on 06/08/2017.
+ */
+
+public interface userService
+{
+    public static final String ENDPOINT_LOCAL = "http://localhost:8888/git/api_EverydayDrinking/web";
+    public static final String ENDPOINT_SERVER = "http://192.168.1.254/git/api_EverydayDrinking/web";
+
+    @GET("/users")
+    List<User> getUsers();
+
+    @GET("/user/{id}")
+    List<User> getUser(@Path("id") String id);
+
+    @POST("/user")
+    List<User> addUser(@Field("q") String query);
+
+    @PUT("/user")
+    List<User> editUser(@Field("q")String query);
+
+
+
+
+}
