1bd7da56e582fe109ad15098de9dbad43df119a1
jeremieRoth <rothjeremie@yahoo.fr>
2017-08-15 20:07:27 +0200

    add activity

--- app/src/main/java/com/example/gilian/bars_coop/MainActivity.java
+++ app/src/main/java/com/example/gilian/bars_coop/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.gilian.bars_coop;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.gilian.bars_coop.Entity.Comment;
@@ -17,6 +20,7 @@ import com.example.gilian.bars_coop.services.CommentService;
 import com.example.gilian.bars_coop.services.DrinkService;
 import com.example.gilian.bars_coop.services.EstablishmentService;
 import com.example.gilian.bars_coop.services.LocationService;
+import com.example.gilian.bars_coop.services.MapActivity;
 import com.example.gilian.bars_coop.services.UserService;
 import com.google.gson.JsonElement;
 
@@ -65,7 +69,19 @@ public class MainActivity extends AppCompatActivity {
         getDrink();
         getComment();
         getLocation();
+        Button loginButton = (Button) findViewById(R.id.btnlogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, MapActivity.class);
+                Bundle extra = new Bundle();
+                extra.putString("user", "vador");
+                intent.putExtras(extra);
+                startActivity(intent);
 
+
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/gilian/bars_coop/services/MapActivity.java
@@ -0,0 +1,22 @@
+package com.example.gilian.bars_coop.services;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.gilian.bars_coop.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        TextView text = (TextView) findViewById(R.id.hello);
+        Intent intent=getIntent();
+        Bundle extra=intent.getExtras();
+        String pseudo = extra.getString("user");
+        text.setText("bonjour "+pseudo);
+    }
+}
